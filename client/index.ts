c2e23bb0a0b321d0d7a4f43ca17953f8b89340d9
Bruno Perel <bruno.perel@firstvet.com>
2025-01-29 15:35:07 +0100

    Embed util types

--- client/index.ts
+++ client/index.ts
@@ -3,7 +3,22 @@ import type { CacheOptions } from "axios-cache-interceptor";
 import { io, type Socket } from "socket.io-client";
 import { Ref, ref } from "vue";
 
-export type { Errorable, ScopedError } from "../util";
+export type ScopedError<ErrorKey extends string = string> = {
+  error: ErrorKey;
+  message: string;
+  selector: string;
+};
+
+export type EitherOr<A, B> = A | B extends object
+  ?
+      | (A & Partial<Record<Exclude<keyof B, keyof A>, never>>)
+      | (B & Partial<Record<Exclude<keyof A, keyof B>, never>>)
+  : A | B;
+
+export type Errorable<T, ErrorKey extends string> = EitherOr<
+  T,
+  EitherOr<{ error: ErrorKey; errorDetails?: string }, ScopedError<ErrorKey>>
+>;
 
 export type WithoutError<T> = T extends { error: any; errorDetails?: any }
   ? never
